c7e8e0f483b40eead52d6ea6f17b7bc692880fac
yryryk <tovchennikov@e1.ru>
2022-12-25 22:46:01 +0500

    feat: добавить Api

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,34 @@
+import api from '../utils/Api';
+import { useState,  useEffect} from 'react';
+
 function Main(props) {
   const {onEditProfile, onAddPlace, onEditAvatar} = props.handlers;
- 
+
+  const [userName, setUserName] = useState('giq');
+  const [userDescription, setUserDescription] = useState('sr');
+  const [userAvatar, setUserAvatar] = useState('../../../images/image.jpg');
+
+  useEffect(() => {
+    api.getUserInfo()
+    .then((result) => {
+      setUserName(result.name);
+      setUserDescription(result.about);
+      setUserAvatar(result.avatar);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  },[]);
+
   return (
     <main>
       <section className="profile">
         <div className="profile__user">
-          <button onClick={onEditAvatar} aria-label="кнопка добавить" type="button" className="profile__user-picture"><div className="profile__user-pen-editing"></div></button>
+          <button onClick={onEditAvatar} aria-label="кнопка добавить" type="button" className="profile__user-picture" style={{ backgroundImage: `url(${userAvatar})` }}><div className="profile__user-pen-editing"></div></button>
           <div className="profile__user-info">
-            <h1 className="profile__title">Жак-Ив Кусто</h1>
+            <h1 className="profile__title">{userName}</h1>
             <button onClick={onEditProfile} aria-label="кнопка редактировать" type="button" className="button-opacity profile__edit-button"></button>
-            <p className="profile__subtitle">Исследователь океана</p>
+            <p className="profile__subtitle">{userDescription}</p>
           </div>
         </div>
         <button onClick={onAddPlace} aria-label="кнопка добавить" type="button" className="button-opacity profile__add-button"></button>
---
+++ src/utils/Api.js
@@ -0,0 +1,97 @@
+class Api {
+  constructor({baseUrl, headers}) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _checkExecution(resolve) {
+    if (resolve.ok) {
+      return resolve.json();
+    }
+    return Promise.reject(`Ошибка: ${resolve.status}`);
+  }
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers
+    })
+    .then(this._checkExecution)
+  }
+
+  setUserInfo(inputValues) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: inputValues.name,
+        about: inputValues.about
+      })
+    })
+    .then(this._checkExecution);
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers
+    })
+    .then(this._checkExecution)
+  }
+
+  setCard(inputValues) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: inputValues.name,
+        link: inputValues.link
+      })
+    })
+    .then(this._checkExecution);
+  }
+
+  deleteCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+    .then(this._checkExecution);
+  }
+
+  addLike(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: 'PUT',
+      headers: this._headers
+    })
+    .then(this._checkExecution);
+  }
+
+  deleteLike(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+    .then(this._checkExecution);
+  }
+
+  setUserAvatar(inputValues) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: inputValues.avatar
+      })
+    })
+    .then(this._checkExecution);
+  }
+}
+
+
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-54',
+  headers: {
+    authorization: 'a77c8be0-2dca-4e0d-816d-247a8a434831',
+    'Content-Type': 'application/json'
+  }
+});
+
+export default api
