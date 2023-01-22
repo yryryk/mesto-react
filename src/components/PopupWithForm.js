56e67d30594b57e120a2905b26484129ec48bdb0
yryryk <tovchennikov@e1.ru>
2023-01-22 23:40:33 +0500

    feat: добавить закрытие по клику на оверлей

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,7 +1,13 @@
 function PopupWithForm(props) {
 
+  function handleOverlayClose (evt) {
+    if (evt.target.classList.contains('popup')) {
+      props.onClose()
+    }
+  }
+  
   return (
-    <div className={`popup popup_type_${props.name} ${props.isOpen&&'popup_open'}`} id={`popup-${props.name}`}>
+    <div className={`popup popup_type_${props.name} ${props.isOpen&&'popup_open'}`} id={`popup-${props.name}`} onClick={handleOverlayClose}>
       <div className="popup__window">
         <button aria-label="кнопка закрытия" type="button" className="button-opacity popup__close-button" onClick={props.onClose}></button>
         <h2 className="popup__title">{props.title}</h2>
