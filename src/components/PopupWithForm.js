2b8dc85ad9b997a7c0f0a33f3879b6888b4cf1fb
yryryk <tovchennikov@e1.ru>
2023-01-22 23:50:35 +0500

    fix: убрать лишнее

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,13 +5,13 @@ function PopupWithForm(props) {
       props.onClose()
     }
   }
-  
+
   return (
     <div className={`popup popup_type_${props.name} ${props.isOpen&&'popup_open'}`} id={`popup-${props.name}`} onClick={handleOverlayClose}>
       <div className="popup__window">
         <button aria-label="кнопка закрытия" type="button" className="button-opacity popup__close-button" onClick={props.onClose}></button>
         <h2 className="popup__title">{props.title}</h2>
-        <form className="popup__form" name={props.name} onSubmit={props.onSubmit} noValidate>
+        <form className="popup__form" name={props.name} onSubmit={props.onSubmit}>
           {props.children}
           <button aria-label="кнопка сохранить" type="submit" className="popup__submit-button">{props.buttonText}</button>
         </form>
